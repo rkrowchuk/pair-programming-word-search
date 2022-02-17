25e0394037976faa0c7b0288e1fce36d407e2469
rkrowchuk <rhiannonkrowchuk@gmail.com>
2022-02-16 18:33:36 -0800

    complete pair assignment with Vince

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,24 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+// pair programming with Rhiannon & Vince
+const wordSearch = (letters, word) => {
+    if (letters.length < 1 || word.length < 1) return false;
+  
+    const horizontalJoin = letters.map((letterSearch) => letterSearch.join(""));
+    const verticalJoin = letters[0].map((col, index) =>
+      letters.map((row) => row[index])
+    );
+  
+    const verticalSearch = verticalJoin.map((letterSearch) =>
+      letterSearch.join("")
+    );
+  
+    for (let letter of horizontalJoin) {
+      if (letter.includes(word)) return true;
+      else {
+        for (let letter of verticalSearch) {
+          if (letter.includes(word)) return true;
+        }
+      }
     }
-}
-
-module.exports = wordSearch
+    return false;
+  };
+  module.exports = wordSearch;
